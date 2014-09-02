1c41c2a88e6b0dae47b67605a5c890d5eaceddfd
MartiniMoe <martini.moe@cheex.de>
2014-09-02 13:38:18 +0200

    Changes from csde_rats

--- dashboard.py
+++ dashboard.py
@@ -2,18 +2,17 @@
 import sys
 import json
 import urllib.request
-import time
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtQuick import *
 
 
-class UpdateThread(QThread):
+class Dashboard(QObject):
 
     temperatureUpdated = pyqtSignal(str)
 
     def __init__(self):
-        super(UpdateThread, self).__init__()
+        super().__init__()
         self.view = QQuickView()
         self.view.setWidth(1024)
         self.view.setHeight(600)
@@ -21,26 +20,34 @@ class UpdateThread(QThread):
         self.view.setResizeMode(QQuickView.SizeRootObjectToView)
         self.view.setSource(QUrl('dashboard.qml'))
         self.view.show()
-        self.qml_rectangle = self.view.rootObject()
-        self.temperatureUpdated.connect(self.qml_rectangle.updateTemperature)
+        self.qmlRoot = self.view.rootObject()
 
-    def run(self):
-        print("Thread starting")
-        while True:
-            time.sleep(1.0)
-            print("Updating")
-            try:
-                bytedata = urllib.request.urlopen('http://status.hasi.it/spaceapi', None, 10).read()
-                stringdata = bytedata.decode("utf-8")
-                jsondata = json.loads(stringdata)
-                self.currentTemperature = str(jsondata['sensors']['temperature'][0]['value']) + " " + jsondata['sensors']['temperature'][0]['unit']
-                self.temperatureUpdated.emit("Temperatur: " + self.currentTemperature)
-            except Exception as e:
-                self.temperatureUpdated.emit("Temperatur: Error")
-                print(e)
+        self.update_thread = QThread()
+        self.update_timer = QTimer()
+        self.update_timer.moveToThread(self.update_thread)
+        self.update_timer.setInterval(1000)
+        self.update_timer.timeout.connect(self.update)
+        self.update_thread.started.connect(self.update_timer.start)
+        self.update_thread.start()
+
+        self.temperatureUpdated.connect(self.qmlRoot.updateTemperature)
+
+    @pyqtSlot()
+    def update(self):
+        print("Updating")
+
+        try:
+            bytedata = urllib.request.urlopen('http://status.hasi.it/spaceapi', None, 10).read()
+            stringdata = bytedata.decode("utf-8")
+            jsondata = json.loads(stringdata)
+            self.currentTemperature = str(jsondata['sensors']['temperature'][0]['value']) + " "
+            self.currentTemperature += jsondata['sensors']['temperature'][0]['unit']
+            self.temperatureUpdated.emit("Temperatur: " + self.currentTemperature)
+        except Exception as e:
+            self.temperatureUpdated.emit("Temperatur: Error")
+            print(e)
+
 
 app = QGuiApplication(sys.argv)
-t = UpdateThread()
-t.start()
-
+board = Dashboard()
 sys.exit(app.exec_())
